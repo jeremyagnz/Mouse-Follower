d6b151b7fea75c56a5f012123b75bb62d1a08a36
Jeremy Arias <jeremyagnz@gmail.com>
2024-11-05 01:59:20 -0400

    Aplicando el useEfect y el setMounted para visualizar componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from "react"
 
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-function App() {
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log({ clientX, clientY });
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    };
+  }, [enabled])
 
   return (
-    <h3>Mouse Follower</h3>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
+
+      </div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+  const [mounted, setMounted] = useState(true);
+
+  return (
+    
+    <main>
+      {mounted && <FollowMouse/>}
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle Mounted FollowMouse
+      </button>
+    </main>
   )
 }
 
